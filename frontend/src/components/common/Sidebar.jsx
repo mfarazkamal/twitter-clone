51cfc21b407b09e7c492c55ee147d9cf5f338453
MFarazKamal <mohdfarazkamal@gmail.com>
2024-11-23 07:24:11 +0530

    posts and fetch post

--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -5,7 +5,7 @@ import { IoNotifications } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { BiLogOut } from "react-icons/bi";
-import {  QueryClient, useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import {   useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 
 
